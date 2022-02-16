f4e41a1b52b60a4f6c3b64aeddb28b3247aa06fc
Limbh97 <usr852963@naver.com>
2022-02-16 10:52:38 +0900

    회원가입 페이지 수정

--- src/main/java/com/icia/storeproject/entity/MemberEntity.java
+++ src/main/java/com/icia/storeproject/entity/MemberEntity.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
-@Table(name = "m_table")
+@Table(name = "member_table")
 public class MemberEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
